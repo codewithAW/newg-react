b28af8f6006e7f74c77fb4e09a5f7a9957372af7
codewithAW <codewithabdulwaheed@gmail.com>
2026-08-29 11:19:51 +0500

    home changes made logo changed

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react'
 import { NavIcons } from '../../utils/icons'
 import './Logo.css'
-import logoImg from "../../assets/images/logo.webp";
+import logoImg from "../../assets/images/newlogo.jpg";
 function Logo() {
   const [showPopup, setShowPopup] = useState(false)
 
   return (
-    <div className="glass-left">
+    <div className="glass-left logo-container-hover">
       <img
         className="logoimg"
         src={logoImg}
         alt="Logo"
         onClick={() => setShowPopup(!showPopup)}
       />
+      <span className="logo-hover-text">Abdulwaheed Software</span>
       <div className={`logo-popup ${showPopup ? 'active' : ''}`}>
         <img className="logoimg" src={logoImg} alt="Logo" />
       </div>
